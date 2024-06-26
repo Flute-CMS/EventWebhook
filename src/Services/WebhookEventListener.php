45127757d312a79c2c5e55dfe5d8828a569f4e24
Nikita <62756604+FlamesONE@users.noreply.github.com>
2024-06-26 17:16:58 +0500

    v1.0.4 fix new v

--- src/Services/WebhookEventListener.php
+++ src/Services/WebhookEventListener.php
@@ -33,9 +33,11 @@ class WebhookEventListener
 
     private static function createWebhookMessage(EventWebhook $eventWebhook, $eventInstance): DiscordEmbedMessage
     {
+        $user = method_exists($eventInstance, 'getUser') ? $eventInstance->getUser() : user()->getCurrentUser();
+
         $message = new DiscordEmbedMessage();
         $message->setUsername($eventWebhook->webhook_name)
-            ->setContent(ContentParser::replaceContent($eventWebhook->content, $eventInstance))
+            ->setContent(ContentParser::replaceContent($eventWebhook->content, $eventInstance, $user))
             ->setAvatar($eventWebhook->webhook_avatar);
 
         $embedsData = Json::decode($eventWebhook->embeds, Json::FORCE_ARRAY);
@@ -43,19 +45,19 @@ class WebhookEventListener
         foreach ($embedsData as $embedData) {
             $embed = new DiscordEmbedMessage();
 
-            $embed->setTitle(ContentParser::replaceContent($embedData['body']['title'] ?? '', $eventInstance))
-                ->setDescription(ContentParser::replaceContent($embedData['body']['description'] ?? '', $eventInstance))
+            $embed->setTitle(ContentParser::replaceContent($embedData['body']['title'] ?? '', $eventInstance, $user))
+                ->setDescription(ContentParser::replaceContent($embedData['body']['description'] ?? '', $eventInstance, $user))
                 ->setUrl($embedData['body']['url'] ?? '')
                 ->setColorWithHexValue($embedData['body']['color'] ?? 'ffffff');
 
             if (isset($embedData['author'])) {
-                $embed->setAuthorName(ContentParser::replaceContent($embedData['author']['name'] ?? '', $eventInstance))
+                $embed->setAuthorName(ContentParser::replaceContent($embedData['author']['name'] ?? '', $eventInstance, $user))
                     ->setAuthorUrl($embedData['author']['url'] ?? '')
                     ->setAuthorIcon($embedData['author']['icon'] ?? '');
             }
 
             if (isset($embedData['footer'])) {
-                $embed->setFooterText(ContentParser::replaceContent($embedData['footer']['text'] ?? '', $eventInstance))
+                $embed->setFooterText(ContentParser::replaceContent($embedData['footer']['text'] ?? '', $eventInstance, $user))
                     ->setFooterIcon($embedData['footer']['icon'] ?? '');
 
                 if (isset($embedData['footer']['time'])) {
@@ -74,8 +76,8 @@ class WebhookEventListener
             if (isset($embedData['fields']) && is_array($embedData['fields'])) {
                 foreach ($embedData['fields'] as $field) {
                     $embed->addField(
-                        ContentParser::replaceContent($field['name'] ?? '', $eventInstance),
-                        ContentParser::replaceContent($field['value'] ?? '', $eventInstance),
+                        ContentParser::replaceContent($field['name'] ?? '', $eventInstance, $user),
+                        ContentParser::replaceContent($field['value'] ?? '', $eventInstance, $user),
                         $field['inline'] ?? false
                     );
                 }
